e32199312806f943b74d42533fe0895998bc360b
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-12-02 10:10:59 -0300

    feat: add Register page

---
+++ src/screens/Register.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, VStack } from "native-base";
+
+import { Header } from "../components/Header";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
+export function Register() {
+  return (
+    <VStack flex={1} p={6} bg="gray.600">
+      <Header title="Nova solicitaçao" />
+      <Input placeholder="Número do patrimônio" mt={4} />
+      <Input
+        placeholder="Descrição do problema"
+        flex={1}
+        mt={5}
+        multiline
+        textAlignVertical="top"
+      />
+      <Button title="Cadastrar" mt={5} />
+    </VStack>
+  );
+}
